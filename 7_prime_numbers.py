31e622b1b1284657d253886d4e3ec195305675c4
Gavrila Dan <gavriladanvalentin@gmail.com>
2026-04-24 18:57:00 +0300

    prime_numbers
    prime_numbers

---
+++ 7_prime_numbers.py
@@ -0,0 +1,10 @@
+for x in range(2, 101):
+    is_prime = True
+
+    for i in range(2, x):
+        if x % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(x)
