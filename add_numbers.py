170c30b5b70037054d043f93148f037be8433072
Gavrila Dan <gavriladanvalentin@gmail.com>
2026-04-24 16:06:18 +0300

    add sum numbers script
    add sum numbers script

---
+++ add_numbers.py
@@ -0,0 +1,14 @@
+total = 0
+for x in range(1, 101):
+    total += x
+print(total)
+
+
+# def sum():
+#     total = 0
+#     for x in range(1, 101):
+#         total += x
+#     return total
+
+
+# print(sum())
